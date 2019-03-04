8f20be945a8236d5eedb144afbbc59fef21616f9
Michael Hoffman <mhoffman999@gmail.com>
2019-03-04 17:11:21 -0500

    Added assets and created dummy static data

---
+++ Splitter/Splitter/Model/Images+Names+Colors.swift
@@ -0,0 +1,31 @@
+//
+//  Images+Names+Colors.swift
+//  Splitter
+//
+//  Created by Michael Hoffman on 3/4/19.
+//  Copyright © 2019 Here We Go. All rights reserved.
+//
+
+import UIKit
+
+//Will use these categories for the tableview index path
+
+//Images
+let camera = UIImage(named: "1")!
+let city = UIImage(named: "2")!
+let buffalo = UIImage(named: "3")!
+let bouquet = UIImage(named: "4")!
+let stand = UIImage(named: "5")!
+let urban = UIImage(named: "6")!
+
+let imageArray = [camera, city, buffalo, bouquet, stand, urban]
+
+//Names
+let namesArray = ["CAMERAS", "CITIES", "ANIMALS", "FLOWERS", "STANDS", "URBAN"]
+
+//Colors
+let colorsArray = [#colorLiteral(red: 1, green: 0.4932718873, blue: 0.4739984274, alpha: 1), #colorLiteral(red: 0.3705204725, green: 0.6517646909, blue: 1, alpha: 1), #colorLiteral(red: 0.4500938654, green: 0.9813225865, blue: 0.4743030667, alpha: 1), #colorLiteral(red: 1, green: 0.8323456645, blue: 0.4732058644, alpha: 1), #colorLiteral(red: 0.476841867, green: 0.5048075914, blue: 1, alpha: 1), #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)]
+
+
+
+
